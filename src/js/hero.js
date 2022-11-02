511b226b357d0cf9e98b4af654792a0912d3575e
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2022-11-02 19:28:52 +0200

    added slider in hero section

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,5 +1,14 @@
 import { refs } from './common-ref';
 
 refs.heroBtn.addEventListener('click', () => console.log('go to ...'));
-refs.heroArrowLeft.addEventListener('click', () => console.log('left'));
-refs.heroArrowRight.addEventListener('click', () => console.log('right'));
+
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = new ItcSimpleSlider('.itcss');
+
+  document.querySelector('.hero__icon--left').onclick = () => {
+    slider.prev();
+  };
+  document.querySelector('.hero__icon--right').onclick = () => {
+    slider.next();
+  };
+});
